a5bd30421afbbc1240bbebaed93e49a8d87c5e3d
Sourabh Kumawat <sourabh.kumawat@ucdconnect.ie>
2026-08-28 15:31:03 -0700

    fix ensemble classes

--- ensemble_models.py
+++ ensemble_models.py
@@ -28,8 +28,8 @@ CLASS_NAMES = [
     "Pericardial effusion", "Coronary artery wall calcification", "Hiatal hernia",
     "Lymphadenopathy", "Emphysema", "Atelectasis", "Lung nodule",
     "Lung opacity", "Pulmonary fibrotic sequela", "Pleural effusion",
-    "Mosaic attenuation pattern", "Peribronchial thickening", "Air trapping",
-    "Bronchiectasis", "Pleural thickening",
+    "Mosaic attenuation pattern", "Peribronchial thickening", "Consolidation",
+    "Bronchiectasis", "Interlobular septal thickening",
 ]
 
 # 8-fold TTA: dim 2=D, 3=H, 4=W in a (B,1,D,H,W) batch tensor.
